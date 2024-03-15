3ec2b153194952d1ba0df8326337d1648fe32322
mattomar <moddathero@gmail.com>
2024-03-15 16:32:02 +0700

    prepared the webpack template

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "to-do-list",
+            filename: "index.html",
+            template: 'src/template.html'
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/style.css', to: 'style.css' },
+                { from: 'src/images', to: 'images' } // Add this line for copying image files
+            ],
+        }),
+    ],
+};
